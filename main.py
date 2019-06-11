9688196d833e7f1439ac02673b1aceba739317e7
Christopher Grabski <grabc59@gmail.com>
2019-06-10 19:49:02 -0700

    prompts each user for hit or stay

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ class Deck:
         for i in self.card_components["suits"]:
             for j in self.card_components["values"]:
                 self.cards.append([i, j])
-
     # return a list of cards to be dealt
+
     def deal(self, qty):
         cards_to_deal = []
         for i in range(qty):
@@ -44,10 +44,10 @@ class Hand:
 
 class Player:
     # Player class
-    def __init__(self, player_type, deck):
+    def __init__(self, name, deck):
         self.bank = 100
         self.hand = []
-        self.player_type = player_type
+        self.name = name
         # deal 2 cards to each player
         self.hit(deck, 2)
 
@@ -75,16 +75,37 @@ dealer = Player("dealer", our_deck)
 player1 = Player("human", our_deck)
 player_list = [dealer, player1]
 
-print('Let\'s play Blackjack')
+print('~~ Let\'s play Blackjack')
+print('~~ Closest to 21 wins. Ace is 1 or 11.')
 
 # run the game until an end condition is met
 while True:
 
-    # prompt players for input, they can hit (get another card) or stay (take no more cards)
+    for player in player_list:
+        print(f'{player.name}: {player.hand}')
+
+    for player in player_list:
+
+        # say whose turn it is
+        print(f'~~ {player.name}\'s turn')
+
+        # prompt players for input, they can hit (get another card) or stay (take no more cards)
+        player_choice = input('Stay (s) / Hit (h)')
+
+        # player_choice logic
+        if player_choice == 's':
+            # stay
+            # move on to next player
+            continue
+        elif player_choice == 'h':
+            # hit
+            # add a card to this player's hand
+            player.hit(our_deck, 1)
+        else:
+            # unrecognized input
+            print(
+                f'~~ What was that? "{player_choice}?" Press "s" if you want to stay and not take any more cards. Press "h" if you want another card.')
 
     # place bets
 
-    for player in player_list:
-        print(player.hand)
-
-    break
+    # break
