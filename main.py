abe81de3cc0e95a0bf45d3ae8b27de1e29eadb28
Christopher Grabski <grabc59@gmail.com>
2019-06-12 20:34:09 -0700

    prompts for additional players

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Deck:
         # these are the possible combinations that make up the deck
         self.card_components = {
             "suits": ["Diamonds", "Hearts", "Clubs", "Spades"],
-            "values": [2, 3, 4, 5, 6, 7, 8, 9, "Jack", "Queen", "King", "Ace"]
+            "values": [2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K", "A"]
         }
 
         self.create_cards()
@@ -71,60 +71,89 @@ class Game:
     # Game class. A place for miscellaneous methods related to the game.
     def __init__(self):
         self.deck = Deck()
+        self.player_list = []
 
-    def print_player_hands(self, player_list):
+    # return a list of player names
+    def get_player_names(self, player_list):
+        player_names = []
         for player in player_list:
+            player_names.append(player.name)
+        return player_names
+
+    # get user input on who's playing
+    def populate_players(self):
+        while True:
+            if len(self.player_list) == 0:
+                # if there are no players yet...
+                new_player_name = input(
+                    f'~~ Who\'s in? (type one player at a time)')
+            else:
+                # to add more players...
+                new_player_name = input(f'~~ Anyone else? (type "no", if not)')
+            # exit player input prompt
+            if new_player_name == "no":
+                break
+            else:
+                # add the entered player name to the player list
+                new_player = Player(new_player_name, self.deck)
+                self.player_list.append(new_player)
+            print(self.get_player_names(self.player_list))
+
+        # add the dealer last
+        dealer = Player("dealer", self.deck)
+        self.player_list.append(dealer)
+
+    def print_player_hands(self):
+        for player in self.player_list:
             print(f'{player.name}: {player.hand}')
+
+    def start(self):
+        while True:
+            # initial display of hands
+            self.print_player_hands()
+            # start of turn
+            for player in self.player_list:
+                # say whose turn it is
+                print(f'~~ {player.name}\'s turn')
+                # player can hit or stay
+                # check if the user's hit/stay input is valid
+                valid_move_choice = False
+                while valid_move_choice == False:
+                    # prompt players for input, they can hit (get another card) or stay (take no more cards)
+                    player_move_choice = input('Stay (s) / Hit (h)')
+                    if player_move_choice == 's':
+                        # stay
+                        # move on to next player
+                        valid_move_choice = True
+                    elif player_move_choice == 'h':
+                        # hit
+                        # add a card to this player's hand and move on to the next player
+                        valid_move_choice = True
+                        player.hit(self.deck, 1)
+                    else:
+                        # unrecognized input
+                        print(
+                            f'~~ What was that? "{player_move_choice}?" Press "s" if you want to stay and not take any more cards. Press "h" if you want another card.')
+
+                # calculate the player hand total
+            # player turns ends here
+
+            # place bets
+
+            # break
 
 
 this_game = Game()
 
-# create a deck
-# our_deck = Deck()
-
-# create players
-dealer = Player("dealer", this_game.deck)
-player1 = Player("human", this_game.deck)
-player_list = [dealer, player1]
+# create players (now done in game.get_players)
+# dealer = Player("dealer", this_game.deck)
+# player1 = Player("human", this_game.deck)
+# player_list = [dealer, player1]
 
 print('~~ Let\'s play Blackjack')
 print('~~ Closest to 21 wins. Ace is 1 or 11.')
 
+this_game.populate_players()
+
 # run the game until an end condition is met
-while True:
-
-    this_game.print_player_hands(player_list)
-
-    for player in player_list:
-
-        # say whose turn it is
-        print(f'~~ {player.name}\'s turn')
-
-        # player_choice logic
-
-        # a way to check if the user's hit/stay input is valid
-        valid_move_choice = False
-
-        while valid_move_choice == False:
-
-            # prompt players for input, they can hit (get another card) or stay (take no more cards)
-            player_move_choice = input('Stay (s) / Hit (h)')
-
-            if player_move_choice == 's':
-                # stay
-                # move on to next player
-                valid_move_choice = True
-            elif player_move_choice == 'h':
-                # hit
-                # add a card to this player's hand
-                print(valid_move_choice, player_move_choice, player.name)
-                valid_move_choice = True
-                player.hit(this_game.deck, 1)
-            else:
-                # unrecognized input
-                print(
-                    f'~~ What was that? "{player_move_choice}?" Press "s" if you want to stay and not take any more cards. Press "h" if you want another card.')
-
-    # place bets
-
-    # break
+this_game.start()
